f589f1fbf64051ce5087af32c968954bc284b2dc
Tristan12x <tristandardiat1208@gmail.com>
2024-09-06 10:21:23 -0400

    Traduction

--- front/ConnectionPage.py
+++ front/ConnectionPage.py
@@ -14,12 +14,12 @@ class ConnectionPage:
         self.frame.grid_rowconfigure(2, weight=1)
         self.frame.grid_rowconfigure(3, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
-        self.label = ttkb.Label(self.frame, text="Please enter your email")
+        self.label = ttkb.Label(self.frame, text="Veuillez entrer votre adresse mail")
         self.label.grid(row=0, column=0, sticky="s")
         self.accountVar = ttkb.StringVar(self.frame, value="")
         self.entry = ttkb.Entry(self.frame, textvariable=self.accountVar)
         self.entry.grid(row=1, column=0)
-        self.button = ttkb.Button(self.frame, text="Register", command=self.register)
+        self.button = ttkb.Button(self.frame, text="Se connecter", command=self.register)
         self.button.grid(row=2, column=0, sticky="n")
         self.errorVar = ttkb.StringVar(self.frame, value="")
         self.errorLabel = ttkb.Label(self.frame, textvariable=self.errorVar)
@@ -31,9 +31,9 @@ class ConnectionPage:
         if userFound != []:
             x = userFound[0]
             if x.get("role") == "COACH":
-                print("Connected")
+                print("Connecté")
                 self.userConnected = x.id
             else:
-                self.errorVar.set("Error : you need a coach account")
+                self.errorVar.set("Erreur : vous avez besoin d'un compte entraîneur")
         else:
-            self.errorVar.set("Error : this user doesn't exist")
+            self.errorVar.set("Erreur : cet utilisateur n'existe pas")
