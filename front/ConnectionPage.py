d57463e6e5adc6ed83d0cf89e38ca7114da06c3a
Tristan12x <tristandardiat1208@gmail.com>
2024-09-19 10:05:06 -0400

    Normalisation et données scalaires modèles

--- front/ConnectionPage.py
+++ front/ConnectionPage.py
@@ -1,3 +1,4 @@
+from tkinter.font import BOLD, Font
 import ttkbootstrap as ttkb
 from core.database.DatabaseManager import DatabaseManager
 
@@ -14,15 +15,18 @@ class ConnectionPage:
         self.frame.grid_rowconfigure(2, weight=1)
         self.frame.grid_rowconfigure(3, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
-        self.label = ttkb.Label(self.frame, text="Veuillez entrer votre adresse mail")
+        labelFont = Font(self.root, size=15, weight=BOLD)
+        self.label = ttkb.Label(self.frame, text="Veuillez entrer votre adresse mail", font=labelFont)
         self.label.grid(row=0, column=0, sticky="s")
         self.accountVar = ttkb.StringVar(self.frame, value="")
         self.entry = ttkb.Entry(self.frame, textvariable=self.accountVar)
         self.entry.grid(row=1, column=0)
-        self.button = ttkb.Button(self.frame, text="Se connecter", command=self.register)
+        buttonStyle = ttkb.Style()
+        buttonStyle.configure('home.TButton', font=Font(self.frame, size=20, weight=BOLD))
+        self.button = ttkb.Button(self.frame, text="Se connecter", style="home.TButton", command=self.register)
         self.button.grid(row=2, column=0, sticky="n")
         self.errorVar = ttkb.StringVar(self.frame, value="")
-        self.errorLabel = ttkb.Label(self.frame, textvariable=self.errorVar)
+        self.errorLabel = ttkb.Label(self.frame, textvariable=self.errorVar, font=labelFont)
         self.errorLabel.grid(row=3, column=0)
         self.frame.grid(sticky="nswe")
     
